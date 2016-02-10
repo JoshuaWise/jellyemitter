38ee30d1b74ffe6d56b13201cb516820458f3540
Joshua Wise <josh@joshuawise.ninja>
2016-02-10 02:59:27 -0500

    added fake tests

---
+++ test/test.js
@@ -0,0 +1,11 @@
+var expect = require('chai').expect
+var JellyEmitter = require('../.')
+
+describe('JellyEmitter', function () {
+	it('should not throw', function () {
+		throw 5
+	})
+	it('should be good', function () {
+		
+	})
+})
