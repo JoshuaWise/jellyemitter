2e983bd287e00dcb3008bda4b4723d1c0b4baed3
Joshua Wise <josh@joshuawise.ninja>
2016-02-11 13:38:42 -0500

    optimized performance for emit() and _removeAllListeners()

--- jellyemitter.js
+++ jellyemitter.js
@@ -2,15 +2,20 @@
 
 function JellyEmitter() {}
 JellyEmitter.prototype = {
-	emit: function (eventName, a, b, c) {
+	emit: function (eventName) {
 		var list = this._events && this._events[eventName]
 		if (list) {
 			var argLen = arguments.length
-			argLen <= 1 ? arg0(this, list) :
-			argLen == 2 ? arg1(this, list, a) :
-			argLen == 3 ? arg2(this, list, a, b) :
-			argLen == 4 ? arg3(this, list, a, b, c) :
-			              argX(this, list, copy(arguments, 1))
+			if (argLen < 5) {
+				argLen <= 1 ? ar0(this, list) :
+				argLen == 2 ? ar1(this, list, arguments[1]) :
+				argLen == 3 ? ar2(this, list, arguments[1], arguments[2]) :
+				              ar3(this, list, arguments[1], arguments[2], arguments[3])
+			} else {
+				var args = new Array(argLen - 1)
+				for (var i=1; i<argLen; i++) {args[i - 1] = arguments[i]}
+				arX(this, list, args)
+			}
 			return true
 		}
 		return false
@@ -73,7 +78,7 @@ JellyEmitter.prototype = {
 			if (arguments.length) {
 				events[eventName] && delete events[eventName]
 			} else {
-				delete this._events
+				this._events = undefined
 			}
 		}
 		return this
@@ -82,64 +87,57 @@ JellyEmitter.prototype = {
 JellyEmitter.prototype.addListener = JellyEmitter.prototype.on
 module.exports = JellyEmitter
 
-function arg0(self, handler) {
+function ar0(self, handler) {
 	if (typeof handler === 'function') {
 		handler.call(self)
 	} else {
-		var listeners = copy(handler, 0)
+		var listeners = handler.slice()
 		for (var i=0, len=listeners.length; i<len; i++) {
 			listeners[i].call(self)
 		}
 	}
 }
 
-function arg1(self, handler, a) {
+function ar1(self, handler, a) {
 	if (typeof handler === 'function') {
 		handler.call(self, a)
 	} else {
-		var listeners = copy(handler, 0)
+		var listeners = handler.slice()
 		for (var i=0, len=listeners.length; i<len; i++) {
 			listeners[i].call(self, a)
 		}
 	}
 }
 
-function arg2(self, handler, a, b) {
+function ar2(self, handler, a, b) {
 	if (typeof handler === 'function') {
 		handler.call(self, a, b)
 	} else {
-		var listeners = copy(handler, 0)
+		var listeners = handler.slice()
 		for (var i=0, len=listeners.length; i<len; i++) {
 			listeners[i].call(self, a, b)
 		}
 	}
 }
 
-function arg3(self, handler, a, b, c) {
+function ar3(self, handler, a, b, c) {
 	if (typeof handler === 'function') {
 		handler.call(self, a, b, c)
 	} else {
-		var listeners = copy(handler, 0)
+		var listeners = handler.slice()
 		for (var i=0, len=listeners.length; i<len; i++) {
 			listeners[i].call(self, a, b, c)
 		}
 	}
 }
 
-function argX(self, handler, args) {
+function arX(self, handler, args) {
 	if (typeof handler === 'function') {
 		handler.apply(self, args)
 	} else {
-		var listeners = copy(handler, 0)
+		var listeners = handler.slice()
 		for (var i=0, len=listeners.length; i<len; i++) {
 			listeners[i].apply(self, args)
 		}
 	}
-}
-
-function copy(arr, start) {
-	var len = arr.length - start;
-	var ret = new Array(len);
-	for (var i=0; i<len; i++) {ret[i] = arr[i + start];}
-	return ret;
 }
